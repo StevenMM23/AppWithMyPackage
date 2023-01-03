4de8f27dc1c6045af9204a6413a6c7805259cbe9
StevenMM23 <esteven123123@outlook.com>
2023-01-03 12:34:27 -0400

    using extention points

--- Program.js
+++ Program.js
@@ -3,6 +3,7 @@ const connection = require("./Database");
 const prompt = require("prompt");
 const configJson = require("console-handler-arquitectura");
 const { personModel } = require("./Models/Persona");
+const path = require("path")
 connection();
 
 prompt.start();
@@ -91,17 +92,17 @@ async function updatePersonByID() {
   }
 }
 //Elimina una person por su ID                          (D)
-async function deletePersonByID() { 
-    try {
-        console.clear()
-        const {personId} = await prompt.get(configJson.getQuestions("delete"));
-        await personModel.findByIdAndDelete({_id: personId});
-        console.log("Deleted !")
-        await consoleWait()
-    } catch (e) {
-        console.log("Oops! Error, try again! ");
-        await consoleWait()
-    }
+async function deletePersonByID() {
+  try {
+    console.clear();
+    const { personId } = await prompt.get(configJson.getQuestions("delete"));
+    await personModel.findByIdAndDelete({ _id: personId });
+    console.log("Deleted !");
+    await consoleWait();
+  } catch (e) {
+    console.log("Oops! Error, try again! ");
+    await consoleWait();
+  }
 }
 //Buscar por ID de la persona
 async function searchByID() {
@@ -130,40 +131,80 @@ async function searchByID() {
   }
 }
 
+async function exportQuestions(name, data){ 
+  await configJson.exportToJSON(name, data);
+}
+
+//Obtiene que funcion esta manejando ese boton
+async function FunctionHandler(button) {
+  const handler = configJson.GetHandlerByButton(button);
+
+  switch (handler.toLowerCase()) {
+    case "createperson":
+      return createPerson();
+    case "readperson":
+      const { answerRead } = await prompt.get(configJson.getQuestions("read"));
+      if (answerRead == "1") return AllPersons();
+      if (answerRead == "2") return searchByID();
+    case "deleteperson":
+      return deletePersonByID();
+    case "updateperson":
+      return updatePersonByID();
+    case "exportperson":
+      return exportQuestions("myData2", await personModel.find({}));
+    case "changepath":
+      return configJson.changePathJSON(
+        path.join(__dirname, "myData2.JSON"),
+        "C:\\Users\\estev\\Desktop\\New folder\\AppWithMyPackage\\Carpeta Demostracion\\myData2.JSON"
+      );
+      await consoleWait();
+    default: {
+      console.log("Handler no encontrado ! ");
+      await consoleWait();
+    }
+  }
+}
 //Renderiza el menu principal del JSON (Button: "Principal'")
 async function Menu() {
   do {
     console.clear();
     const [principalScreen] = configJson.getScreenByButton("principal");
-    configJson.getOptions("principal");
+    configJson.getOptionsColor("magenta", "principal");
 
-    console.log(principalScreen.title);
+    configJson.consoleLogColor("blue", principalScreen.title);
     const { answer } = await menuPrincipal();
-    const x = answer.trim()
+    const x = answer.trim();
     switch (x.toLowerCase()) {
       case "create":
-        await createPerson();
+        await FunctionHandler(x);
         break;
       case "read":
-        console.clear()
-        const { answerRead } = await prompt.get(
-          configJson.getQuestions("read")
-        );
-        if (answerRead == "1") await AllPersons();
-        if (answerRead == "2") await searchByID();
+        await FunctionHandler(x);
         break;
       case "update":
-        await updatePersonByID();
+        await FunctionHandler(x);
         break;
-      case "delete": 
-        await deletePersonByID()
-        break
-        case "ex": 
-        console.clear()
-        console.log("Bye byee !! :D")
+      case "delete":
+        await FunctionHandler(x);
+        break;
+      case "export":
+        await FunctionHandler(x);
+        break;
+      case "change":
+        await FunctionHandler(x);
+        break;
+      case "ex":
+        console.clear();
+        configJson.consoleLogColor("Blue", "Hasta Luego !");
         await consoleWait();
-        process.exit()
-        break
+        process.exit();
+      default:
+        configJson.consoleLogColor(
+          "red",
+          "No se identifico una pantalla con ese boton"
+        );
+        await consoleWait();
+        break;
     }
   } while (true);
 }
